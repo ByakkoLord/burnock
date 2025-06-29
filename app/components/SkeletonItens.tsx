704c5a4efe75b4ed11458c051d9a8316c0a10fa8
ByakkoLord <g.fonsecadacosta@gmail.com>
2025-06-29 03:48:38 -0300

    new skeletons from itens

---
+++ app/components/SkeletonItens.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useRef } from "react";
+import { Animated } from "react-native";
+
+interface SkeletonItensProps {
+  showSkeleton?: boolean;
+}
+
+export default function SkeletonItens({ showSkeleton }: SkeletonItensProps) {
+  const pulseAnim = useRef(new Animated.Value(0.3)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(pulseAnim, {
+          toValue: 1,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+        Animated.timing(pulseAnim, {
+          toValue: 0.3,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
+
+  return (
+    <>
+      {showSkeleton && (
+        <Animated.View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "center",
+            marginTop: 80,
+            gap: 16,
+            opacity: pulseAnim, // Aqui a mágica acontece
+          }}
+        >
+          {[1, 2, 3, 4].map((_, i) => (
+            <Animated.View
+              key={i}
+              style={{
+                width: "80%",
+                height: 119,
+                backgroundColor: "#D8D8D8",
+                borderRadius: 14,
+              }}
+            />
+          ))}
+        </Animated.View>
+      )}
+    </>
+  );
+}
